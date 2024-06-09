fcf99826aa0b28bd08c7121c834cad32fa652937
Cbrnex <31788262+7rs@users.noreply.github.com>
2024-06-09 20:41:16 +0900

    fix workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,30 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        starlight({
-            title: 'Chaos',
-            social: {
-                github: 'https://github.com/7rs/chaos',
-            },
-            sidebar: [
-                {
-                    label: 'Guides',
-                    items: [
-                        // Each item here is one entry in the navigation menu.
-                        { label: 'Example Guide', link: '/guides/example/' },
-                    ],
-                },
-                {
-                    label: 'Reference',
-                    autogenerate: { directory: 'reference' },
-                },
-            ],
-        }),
-    ],
+  site: 'https://7rs.github.io',
+  base: 'chaos',
+  integrations: [starlight({
+    title: 'Chaos',
+    social: {
+      github: 'https://github.com/7rs/chaos'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+    // Each item here is one entry in the navigation menu.
+        {
+          label: 'Example Guide',
+          link: '/guides/example/'
+        }]
+    }, {
+      label: 'Reference',
+        autogenerate: {
+          directory: 'reference'
+        }
+      }]
+  }), tailwind()]
 });
