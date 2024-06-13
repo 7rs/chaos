26b0dec050b74469e8ee61924efeafe7ecce8993
Cbrnex <31788262+7rs@users.noreply.github.com>
2024-06-14 00:43:18 +0900

    add fonts and update some docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,40 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://7rs.github.io',
-  base: "/chaos",
-  markdown: {
-    shikiConfig: {
-      theme: 'dracula',
-    }
-  },
+  base: '/chaos',
   integrations: [
     starlight({
       title: 'Chaos',
       customCss: [
         '@fontsource/fira-sans/400.css',
         '@fontsource/fira-code/400.css',
-        'src/styles/custom.css'
+        '@fontsource/biz-udpgothic/400.css',
+        '@fontsource/biz-udgothic/400.css',
+        'src/styles/custom.css',
       ],
       social: {
-        github: 'https://github.com/7rs/chaos'
+        github: 'https://github.com/7rs/chaos',
       },
       sidebar: [
         { label: 'Guides', autogenerate: { directory: 'guides' } },
         { label: 'FAQ', autogenerate: { directory: 'faq' } },
-        { label: 'Modlist', autogenerate: { directory: 'mods' } }
+        { label: 'Modlist', autogenerate: { directory: 'mods' } },
       ],
-      defaultLocale: "root",
+      defaultLocale: 'root',
       locales: {
         root: { label: 'English', lang: 'en' },
-        'ja': { label: '日本語', lang: 'ja' },
+        ja: { label: '日本語', lang: 'ja' },
+      },
+      expressiveCode: {
+        themes: ['dracula', 'github-light'],
+        defaultProps: { frame: 'none' },
       },
     }),
-    tailwind()
-  ]
+    tailwind(),
+  ],
 });
